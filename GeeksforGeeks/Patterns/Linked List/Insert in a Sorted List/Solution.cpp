2606d75667ab9d31db9582f500761f17daddb6da
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-03 22:19:32 +0530

    Add C++ Solution to Patterns/Linked List

--- GeeksforGeeks/Patterns/Linked List/Insert in a Sorted List/Solution.cpp
+++ GeeksforGeeks/Patterns/Linked List/Insert in a Sorted List/Solution.cpp
@@ -17,6 +17,27 @@ class Solution {
     // Should return head of the modified linked list
     Node* sortedInsert(Node* head, int key) {
         // Code here
-        
+        Node* cur=head;
+        Node* prev=NULL;
+        while(cur){
+            if(key<=cur->data) break;
+            prev=cur;
+            cur=cur->next;
+        }
+        if(cur==head){
+            Node* new_node=new Node(key);
+            new_node->next=cur;
+            head=new_node;
+        }
+        else if(cur==NULL){
+            Node* new_node=new Node(key);
+            prev->next=new_node;
+        }
+        else{
+            Node* new_node=new Node(key);
+            new_node->next=cur;
+            prev->next=new_node;
+        }
+        return head;
     }
 };
