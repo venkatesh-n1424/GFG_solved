d53c0f2da6c18be4de0f43980c91d293d0d9882c
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-03 22:07:04 +0530

    Add C++ Solution to Patterns/Linked List

---
+++ GeeksforGeeks/Patterns/Linked List/Insert in a Sorted List/Solution.cpp
@@ -0,0 +1,22 @@
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Should return head of the modified linked list
+    Node* sortedInsert(Node* head, int key) {
+        // Code here
+        
+    }
+};
