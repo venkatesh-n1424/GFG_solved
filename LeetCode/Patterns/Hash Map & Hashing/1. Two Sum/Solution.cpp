d6a94d21f65d47a0b81b392bcf44e7d110755482
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-06 18:09:53 +0530

    Add C++ Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
+        //brute
         int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    res.emplace_back(i);
-                    res.emplace_back(j);
-                    break;
-                }
-            }
+        // vector<int> res;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums.at(i)+nums.at(j)==target) res = {i,j};
+        //     }
+        // }
+        // return res;
+        //optimal
+        vector<pair<int,int>> val;
+        for(int i=0;i<n;i++) val.emplace_back(nums.at(i),i);
+        sort(val.begin(),val.end());
+        int i=0,j=n-1;
+        while(i<j){
+            if(val[i].first+val[j].first>target) j--;
+            else if(val[i].first+val[j].first<target) i++;
+            else return vector<int> {val[i].second,val[j].second};
         }
-        return res;
+        return {};
     }
 };
