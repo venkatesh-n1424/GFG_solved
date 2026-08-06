25af79eae63888fd6cf60b69ee006da3aa1191b4
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-06 18:06:00 +0530

    Add C++ Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<pair<int,int>> val;
-       vector<int> res;
-       int n=nums.size();
-       for(int i=0;i<n;i++) val.push_back({nums[i],i});
-       sort(val.begin(),val.end());
-       int i=0,j=n-1;
-       while(i<j){
-        if(val[i].first+val[j].first==target){
-            res={val[i].second,val[j].second};
-            break;
+        //brute
+        int n=nums.size();
+        // vector<int> res;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums.at(i)+nums.at(j)==target) res = {i,j};
+        //     }
+        // }
+        // return res;
+        //optimal
+        vector<pair<int,int>> val;
+        for(int i=0;i<n;i++) val.emplace_back(nums.at(i),i);
+        sort(val.begin(),val.end());
+        int i=0,j=n-1;
+        while(i<j){
+            if(val[i].first+val[j].first>target) j--;
+            else if(val[i].first+val[j].first<target) i++;
+            else return vector<int> {val[i].second,val[j].second};
         }
-        else if(val[i].first+val[j].first>target) j--;
-        else i++;
-       }
-       return res;
+        return {};
     }
 };
