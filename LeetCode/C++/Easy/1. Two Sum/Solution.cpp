3e7f8f0e328f37f7277136f370359539c8e7e172
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-06 18:09:40 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1. Two Sum/Solution.cpp
+++ LeetCode/C++/Easy/1. Two Sum/Solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<pair<int,int>> val;
-       vector<int> res;
-       int n=nums.size();
-       for(int i=0;i<n;i++) val.push_back({nums[i],i});
-       sort(val.begin(),val.end());
-       int i=0,j=n-1;
-       while(i<j){
-        if(val[i].first+val[j].first==target){
-            res={val[i].second,val[j].second};
-            break;
+        vector<int> res;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    res.emplace_back(i);
+                    res.emplace_back(j);
+                    break;
+                }
+            }
         }
-        else if(val[i].first+val[j].first>target) j--;
-        else i++;
-       }
-       return res;
+        return res;
     }
 };
