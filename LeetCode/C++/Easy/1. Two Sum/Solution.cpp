14b356f25473adedca369c7e5da4f97250f1b25c
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-06 18:11:01 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1. Two Sum/Solution.cpp
+++ LeetCode/C++/Easy/1. Two Sum/Solution.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sol;
+        //Brute 
+        /*
         int n=nums.size();
-        vector<int> res;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    res={i,j};
-                    break;
-                } 
+                if(nums[i]+nums[j]==target) {
+                    sol.emplace_back(i);
+                     sol.emplace_back(j);
+                     break;
+                }
             }
+        }*/
+        map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            int a=nums[i];
+            int rem=target-a;
+            if(mpp.find(rem)!=mpp.end()){
+                sol.emplace_back(i);
+                sol.emplace_back(mpp[rem]);
+                break;
+            }
+            mpp[a]=i;
         }
-        return res;
+        return sol;
     }
 };
