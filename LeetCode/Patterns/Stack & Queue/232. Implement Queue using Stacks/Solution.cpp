aba7fd9982bcff0d60465162ce00b2c8a85889e0
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-08 17:41:32 +0530

    Add C++ Solution to Patterns/Stack & Queue

---
+++ LeetCode/Patterns/Stack & Queue/232. Implement Queue using Stacks/Solution.cpp
@@ -0,0 +1,44 @@
+class MyQueue {
+public:
+    stack<int> input,output;
+    int peekele=-1;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(input.empty()) peekele=x;
+        input.push(x);
+    }
+    
+    int pop() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int val=output.top();
+        output.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(output.empty()) return peekele;
+        return output.top();
+    }
+    
+    bool empty() {
+        if(input.empty() && output.empty()) return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
