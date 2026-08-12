b247da3ea2d52b4b6e74f316fa5fa6830f80e751
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-12 19:55:57 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/128. Longest Consecutive Sequence/Solution.cpp
+++ LeetCode/C++/Medium/128. Longest Consecutive Sequence/Solution.cpp
@@ -20,33 +20,18 @@ public:
         //     lc=max(c,lc);
         // }
         //better
-        //sort(nums.begin(),nums.end());
-        // int lastsmaller=INT_MIN,cntcur=0;
-        // for(int i:nums){
-        //     if(i-1==lastsmaller){
-        //         cntcur++;
-        //         lastsmaller=i;
-        //     }
-        //     else if(i!=lastsmaller){
-        //         lastsmaller=i;
-        //         cntcur=1;
-        //     }
-        //     lc=max(cntcur,lc);
-        // }
-        set<int> s;
+        sort(nums.begin(),nums.end());
+        int lastsmaller=INT_MIN,cntcur=0;
         for(int i:nums){
-            s.insert(i);
-        }
-        for(int i:nums){
-            if(!s.count(i-1)){
-                int c=1;
-                int x=i;
-                while(s.count(x+1)){
-                    c++;
-                    x++;
-                }
-                lc=max(c,lc);
+            if(i-1==lastsmaller){
+                cntcur++;
+                lastsmaller=i;
             }
+            else if(i!=lastsmaller){
+                lastsmaller=i;
+                cntcur=1;
+            }
+            lc=max(cntcur,lc);
         }
         return lc;
     }
