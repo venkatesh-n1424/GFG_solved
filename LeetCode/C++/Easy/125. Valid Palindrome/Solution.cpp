374794677d1971af02994833cf34e983040849b6
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-14 16:02:59 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/125. Valid Palindrome/Solution.cpp
+++ LeetCode/C++/Easy/125. Valid Palindrome/Solution.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
+    bool ischar(char c){
+        if((c<'A'|| c>'Z')&&(c<'a'||c>'z')&&(c<'0'||c>'9'))
+        return false;
+        return true;
+    }
     bool isPalindrome(string s) {
-        
-        for(int i=0;i<s.size();i++){
-            if((s[i]<'A'||s[i]>'Z')&&(s[i]<'a'||s[i]>'z')&&(s[i]<'0'||s[i]>'9')){
-                s.erase(i,1);
-                i--;
-            }
-        }
-        
-        for(auto& x:s){
-            x=tolower(x);
-        }
-        cout<<s;
-        int i=0,j=s.size()-1;
-        while(i<=j){
-            if(s[i++]!=s[j--]) return false;
+        int l=0,r=s.size()-1;
+        while(l<=r){
+            if(!ischar(s[l])) l++;
+            else if((!ischar(s[r]))) r--;
+            else if(tolower(s[l++])!=tolower(s[r--])) return false;
         }
         return true;
     }
