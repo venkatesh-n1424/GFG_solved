833f4394503e38601154ee084621af7caae97d76
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-28 14:58:26 +0530

    Add Medium C++ Solution for 424. Longest Repeating Character Replacement from LeetCode

--- LeetCode/C++/Medium/424. Longest Repeating Character Replacement/Solution.cpp
+++ LeetCode/C++/Medium/424. Longest Repeating Character Replacement/Solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
+        //brute
+        int n=s.size(),res=1;
+        for(int i=0;i<n;i++){
+            vector<int> f(26,0);
+            int maxf=0;
+            for(int j=i;j<n;j++){
+                maxf=max(maxf,++f[s[j]-'A']);
+                int changes=(j-i+1)-maxf;
+                if(changes<=k){
+                    res=max(res,j-i+1);
+                }
+                else break;
+            }
+        }
+        return res;
     }
 };
