802f2c75edfe6b77e7bed9766010665cf7bd3b77
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-28 14:49:43 +0530

    Add Medium C++ Solution for 424. Longest Repeating Character Replacement from LeetCode

--- LeetCode/C++/Medium/424. Longest Repeating Character Replacement/Solution.cpp
+++ LeetCode/C++/Medium/424. Longest Repeating Character Replacement/Solution.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_set<char> st;
-        int i=0,j=0,n=s.size(),t=k,ans=1;
-        while(j<n){
-            st.insert(s[j]);
-            if(st.size()>1){
-                if(t){
-                st.erase(s[j]);
-                s[j]=s[j-1];
-                t--;
-                }
-                else{
-                    ans=max(ans,j-i);
-                    i=j-1;
-                    st.erase(s[i]);
-                    t=k;
-                }
-            }
-            j++;
-        }
-        return ans=max(ans,j-i);
+        
     }
 };
