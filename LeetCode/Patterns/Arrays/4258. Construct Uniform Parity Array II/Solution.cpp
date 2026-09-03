fcd77522b7d6cb3c1d266bc12646a9f80103781a
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-03 12:40:05 +0530

    Add Medium C++ Solution for 4258. Construct Uniform Parity Array II from LeetCode

--- LeetCode/Patterns/Arrays/4258. Construct Uniform Parity Array II/Solution.cpp
+++ LeetCode/Patterns/Arrays/4258. Construct Uniform Parity Array II/Solution.cpp
@@ -6,16 +6,21 @@ public:
             if(i%2) oc++;
         }
         if(oc==0 || oc==nums1.size()) return true;
-        sort(nums1.begin(),nums1.end(),greater<int>());
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0){
-                if(i==n-1) return false;
-                for(int j=i+1;j<n;j++){
-                    if(nums1[j]%2)
-                    if(nums1[i]-nums1[j]<1) return false;
-                }
-            }
-        }
-        return true;
+        //brute
+        // sort(nums1.begin(),nums1.end(),greater<int>());
+        // for(int i=0;i<n;i++){
+        //     if(nums1[i]%2==0){
+        //         if(i==n-1) return false;
+        //         for(int j=i+1;j<n;j++){
+        //             if(nums1[j]%2)
+        //             if(nums1[i]-nums1[j]<1) return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        //optimal
+        int minele=*min_element(nums1.begin(),nums1.end());
+        if(minele%2) return true;
+        return false;
     }
 };
