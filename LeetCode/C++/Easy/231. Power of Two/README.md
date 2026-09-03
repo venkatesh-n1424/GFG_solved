c6318a813e0114abe05624f575bf22b9977d2eb9
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-03 14:35:01 +0530

    Add Easy C++ Solution for 231. Power of Two from LeetCode

--- LeetCode/C++/Easy/231. Power of Two/README.md
+++ LeetCode/C++/Easy/231. Power of Two/README.md
@@ -8,8 +8,8 @@
 Math, Bit Manipulation, Recursion
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 7.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Easy/231. Power of Two/Solution.cpp
+++ LeetCode/C++/Easy/231. Power of Two/Solution.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && (n & (n-1))==0;
+        while(n!=1){
+            if(n%2==0) n/=2;
+            else return false;
+        }
+        return true;
     }
 };
