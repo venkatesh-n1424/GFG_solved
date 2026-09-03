c800f371fa9edead455b049c2ede5d373dae46e2
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-03 12:25:04 +0530

    Add Medium C++ Solution for 3876. Construct Uniform Parity Array II from LeetCode

---
+++ LeetCode/Patterns/Arrays/3876. Construct Uniform Parity Array II/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int oc=0,n=nums1.size();
+        for(int& i : nums1){
+            if(i%2) oc++;
+        }
+        if(oc==0 || oc==nums1.size()) return true;
+        sort(nums1.begin(),nums1.end(),greater<int>());
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2==0){
+                if(i==n-1) return false;
+                for(int j=i+1;j<n;j++){
+                    if(nums1[i]-nums1[j]<1) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
