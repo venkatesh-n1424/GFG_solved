1e82e3cadf26ad0a0a09f6253670740de2d5be06
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-07 22:47:19 +0530

    Add Medium C++ Solution for 19. Remove Nth Node From End of List from LeetCode

--- LeetCode/C++/Medium/19. Remove Nth Node From End of List/README.md
+++ LeetCode/C++/Medium/19. Remove Nth Node From End of List/README.md
@@ -8,8 +8,8 @@
 Linked List, Two Pointers
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 15 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Medium/19. Remove Nth Node From End of List/Solution.cpp
+++ LeetCode/C++/Medium/19. Remove Nth Node From End of List/Solution.cpp
@@ -11,29 +11,44 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int sz=0;
-        ListNode* cur=head;
-        while(cur){
-            sz++;
-            cur=cur->next;
+        // int sz=0;
+        // ListNode* cur=head;
+        // while(cur){
+        //     sz++;
+        //     cur=cur->next;
+        // }
+        // if(sz==1){
+        //     return nullptr;
+        // }
+        // int pos=sz-n+1;
+        // cur=head;
+        // if(pos==1){
+        //     head=cur->next;
+        //     cur->next=nullptr;
+        //     return head;
+        // }
+        // ListNode* prev=NULL;
+        // while(--pos){
+        //     prev=cur;
+        //     cur=cur->next;
+        // }
+        // prev->next=cur->next;
+        // cur->next=nullptr;
+        // return head;
+        ListNode* f=head,*s=head;
+        for(int i=0;i<n;i++) f=f->next;
+        if(!f){
+            ListNode* newhead=head->next;
+            head->next=nullptr;
+            return newhead;
         }
-        if(sz==1){
-            return nullptr;
+        while(f->next){
+            s=s->next;
+            f=f->next;
         }
-        int pos=sz-n+1;
-        cur=head;
-        if(pos==1){
-            head=cur->next;
-            cur->next=nullptr;
-            return head;
-        }
-        ListNode* prev=NULL;
-        while(--pos){
-            prev=cur;
-            cur=cur->next;
-        }
-        prev->next=cur->next;
-        cur->next=nullptr;
+        ListNode* dnode=s->next;
+        s->next=dnode->next;
+        dnode->next=nullptr;
         return head;
     }
 };
