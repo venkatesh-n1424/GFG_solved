2d44d342b6e55703726c5ed5a750683da5fb8b0b
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-16 15:44:49 +0530

    Add Medium C++ Solution for 1621. Number of Sets of K Non-Overlapping Line Segments from LeetCode

--- LeetCode/C++/Medium/1621. Number of Sets of K Non-Overlapping Line Segments/Solution.cpp
+++ LeetCode/C++/Medium/1621. Number of Sets of K Non-Overlapping Line Segments/Solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int mod=1e9+7;
+    vector<vector<int>> dp;
+    int solve(int n,int k,int i){
+        if(k==0) return 1;
+        if(i>=n) return 0;
+        if(dp[k][i]!=-1) return dp[k][i]%mod;
+        l skip = solve(n,k,i+1)%mod;
+        int take=0;
+        for(int j=i+1;j<n;j++){
+            take=(take+solve(n,k-1,j))%mod;
+        }
+        return dp[k][i]=(skip+take)%mod;
+    }
     int numberOfSets(int n, int k) {
-        
+        dp.resize(1001,vector<int>(1001,-1));
+        return solve(n,k,0);
     }
 };
