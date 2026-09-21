b8f4e39b84e05c97377308a2cf74de431247e329
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-21 12:47:24 +0530

    Add Medium C++ Solution for 3831. Find X Value of Array I from LeetCode

--- LeetCode/Patterns/Dynamic Programming/3831. Find X Value of Array I/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/3831. Find X Value of Array I/Solution.cpp
@@ -4,11 +4,27 @@ public:
         //brute
         int n=nums.size();
         vector<long long> res(k,0);
-        for(int i=0;i<n;i++){
-            long long prod=1;
-            for(int j=i;j<n;j++){
-                prod*=nums[j];
-                res[prod%k]++;
+        // for(int i=0;i<n;i++){
+        //     long long prod=1;
+        //     for(int j=i;j<n;j++){
+        //         prod*=nums[j];
+        //         res[prod%k]++;
+        //     }
+        // }
+        //DP
+        vector<vector<long long>> dp(n,vector<long long>(k,0));
+        //dp[i] [r] = number of subarrays ending at i with product % k == r
+        dp[0][nums[0]%k]=1;
+        for(int i=1;i<n;i++){
+            dp[i][nums[i]%k]=1;
+            for(int r=0;r<k;r++){
+                int col = (r*nums[i])%k;
+                dp[i][col]+=dp[i-1][r];
+            }
+        }
+        for(int r=0;r<k;r++){
+            for(int i=0;i<n;i++){
+                res[r]+=dp[i][r];
             }
         }
         return res;
