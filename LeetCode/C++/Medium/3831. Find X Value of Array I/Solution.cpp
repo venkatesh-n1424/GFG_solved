b4d61876cd02bad2fdb1e2cc36f2b8270789ea03
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-09-21 12:22:41 +0530

    Add Medium C++ Solution for 3831. Find X Value of Array I from LeetCode

---
+++ LeetCode/C++/Medium/3831. Find X Value of Array I/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        //brute
+        int n=nums.size();
+        vector<long long> res(k,0);
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for(int j=i;j<n;j++){
+                prod*=nums[j];
+                res[prod%k]++;
+            }
+        }
+        return res;
+    }
+};
